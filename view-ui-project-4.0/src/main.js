2b15516f56673914fd4ff803e72e6996fc670654
liubin <1324576393@qq.com>
2019-11-19 15:55:50 +0800

    前后台框架

---
+++ view-ui-project-4.0/src/main.js
@@ -0,0 +1,34 @@
+import Vue from 'vue';
+import ViewUI from 'view-design';
+import VueRouter from 'vue-router';
+import Routers from './router';
+import Util from './libs/util';
+import App from './app.vue';
+import 'view-design/dist/styles/iview.css';
+
+Vue.use(VueRouter);
+Vue.use(ViewUI);
+
+// 路由配置
+const RouterConfig = {
+    mode: 'history',
+    routes: Routers
+};
+const router = new VueRouter(RouterConfig);
+
+router.beforeEach((to, from, next) => {
+    ViewUI.LoadingBar.start();
+    Util.title(to.meta.title);
+    next();
+});
+
+router.afterEach((to, from, next) => {
+    ViewUI.LoadingBar.finish();
+    window.scrollTo(0, 0);
+});
+
+new Vue({
+    el: '#app',
+    router: router,
+    render: h => h(App)
+});
