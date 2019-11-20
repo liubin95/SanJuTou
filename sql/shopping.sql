43536f8321ba6953c51be2d9280a031f891f1ac7
liubin <1324576393@qq.com>
2019-11-20 10:30:54 +0800

    表结构重新设计

--- sql/shopping.sql
+++ sql/shopping.sql
@@ -1,214 +1,177 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat MySQL Data Transfer
 
-Source Server         : 本地
-Source Server Version : 50623
-Source Host           : localhost:3306
-Source Database       : shopping
+ Source Server         : 本地
+ Source Server Type    : MySQL
+ Source Server Version : 50641
+ Source Host           : localhost:3306
+ Source Schema         : shopping
 
-Target Server Type    : MYSQL
-Target Server Version : 50623
-File Encoding         : 65001
+ Target Server Type    : MySQL
+ Target Server Version : 50641
+ File Encoding         : 65001
 
-Date: 2019-06-01 11:57:43
+ Date: 20/11/2019 10:28:57
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for addinfo
+-- Table structure for address_info
 -- ----------------------------
-DROP TABLE IF EXISTS `addinfo`;
-CREATE TABLE `addinfo` (
+DROP TABLE IF EXISTS `address_info`;
+CREATE TABLE `address_info`  (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `userid` int(11) NOT NULL DEFAULT '0' COMMENT '用户id',
-  `address` varchar(200) NOT NULL DEFAULT '' COMMENT '详细地址信息',
-  `mobile` varchar(20) NOT NULL DEFAULT '0' COMMENT '电话',
-  `defaultadd` smallint(2) NOT NULL DEFAULT '0' COMMENT '是否默认地址。0代表不是，1代表是',
-  `name` varchar(200) NOT NULL DEFAULT '' COMMENT '收件人姓名',
-  `province` varchar(200) DEFAULT NULL COMMENT '省级',
-  `city` varchar(200) DEFAULT NULL COMMENT '市级',
-  `county` varchar(200) DEFAULT NULL COMMENT '区级',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+  `customer_id` int(11) NOT NULL DEFAULT 0 COMMENT '用户id',
+  `address` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '详细地址信息',
+  `mobile` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '0' COMMENT '电话',
+  `is_default` smallint(2) NOT NULL DEFAULT 0 COMMENT '是否默认地址。0代表不是，1代表是',
+  `name` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '收件人姓名',
+  `province` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '省级',
+  `city` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '市级',
+  `county` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '区级',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '地址信息表' ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Table structure for category
 -- ----------------------------
 DROP TABLE IF EXISTS `category`;
-CREATE TABLE `category` (
+CREATE TABLE `category`  (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `parentid` int(11) DEFAULT NULL COMMENT '父级id',
-  `categoryname` varchar(200) NOT NULL DEFAULT '' COMMENT '类别名',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=58 DEFAULT CHARSET=utf8;
+  `parent_id` int(11) NULL DEFAULT NULL COMMENT '父级id',
+  `category_name` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '类别名',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '分类表' ROW_FORMAT = Compact;
 
 -- ----------------------------
--- Table structure for collectioninfo
+-- Table structure for collection_info
 -- ----------------------------
-DROP TABLE IF EXISTS `collectioninfo`;
-CREATE TABLE `collectioninfo` (
+DROP TABLE IF EXISTS `collection_info`;
+CREATE TABLE `collection_info`  (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `userid` int(11) NOT NULL DEFAULT '0' COMMENT '用户id',
-  `typeid` int(11) NOT NULL DEFAULT '0' COMMENT '0代表店铺，1代表商品',
-  `sidorpid` varchar(200) NOT NULL DEFAULT '0' COMMENT '店铺或商品id',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
+  `customer_id` int(11) NOT NULL DEFAULT 0 COMMENT '用户id',
+  `type_id` int(11) NOT NULL DEFAULT 0 COMMENT '0代表店铺，1代表商品',
+  `sidorpid` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '0' COMMENT '店铺或商品id',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '收藏信息表' ROW_FORMAT = Compact;
 
 -- ----------------------------
--- Table structure for oderinfo
+-- Table structure for oder_info
 -- ----------------------------
-DROP TABLE IF EXISTS `oderinfo`;
-CREATE TABLE `oderinfo` (
+DROP TABLE IF EXISTS `oder_info`;
+CREATE TABLE `oder_info`  (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `typeid` int(11) NOT NULL DEFAULT '0' COMMENT '0代表待付款，1代表已付款，2已收货',
-  `userid` int(11) NOT NULL DEFAULT '0' COMMENT '用户id',
-  `proid` varchar(200) NOT NULL DEFAULT '0' COMMENT '商品id',
-  `counts` int(11) NOT NULL DEFAULT '0' COMMENT '商品数量',
-  `cratedate` datetime DEFAULT NULL COMMENT '创建时间',
-  `paydate` datetime DEFAULT NULL COMMENT '付款时间',
-  `donedate` datetime DEFAULT NULL COMMENT '收货时间',
-  `addressid` int(11) DEFAULT '0' COMMENT '收货地址id',
-  `totalprice` decimal(10,2) NOT NULL COMMENT '总价',
-  `note` varchar(200) DEFAULT '' COMMENT '买家留言',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Table structure for product
--- ----------------------------
-DROP TABLE IF EXISTS `product`;
-CREATE TABLE `product` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `proname` varchar(200) NOT NULL DEFAULT '' COMMENT '商品名称',
-  `oldprice` decimal(10,2) DEFAULT NULL COMMENT '原价',
-  `newprice` decimal(10,2) NOT NULL COMMENT '现价',
-  `cateid` int(11) NOT NULL DEFAULT '0' COMMENT '类别id，对应类别表id',
-  `storeid` int(11) NOT NULL DEFAULT '0' COMMENT '商铺id，对应商铺表id',
-  `status` int(1) NOT NULL DEFAULT '0' COMMENT '商品状态，0未上架，1已经上架',
-  `cover` varchar(200) NOT NULL DEFAULT '' COMMENT '商品图片路径',
-  `stock` int(10) NOT NULL DEFAULT '0' COMMENT '库存',
-  `parentid` varchar(200) DEFAULT NULL COMMENT '父级商品id',
-  `saletime` datetime DEFAULT NULL COMMENT '月销售量统计时间',
-  `sales` int(255) DEFAULT '0' COMMENT '月销售量',
-  `info` varchar(255) DEFAULT NULL COMMENT '一句话简介',
-  `note` varchar(255) DEFAULT NULL COMMENT '子级商品备注信息',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
+  `type_id` int(11) NOT NULL DEFAULT 0 COMMENT '0代表待付款，1代表已付款，2已收货',
+  `customer_id` int(11) NOT NULL DEFAULT 0 COMMENT '用户id',
+  `sku_id` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '0' COMMENT '商品id',
+  `counts` int(11) NOT NULL DEFAULT 0 COMMENT '商品数量',
+  `crate_date` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `pay_date` datetime(0) NULL DEFAULT NULL COMMENT '付款时间',
+  `done_date` datetime(0) NULL DEFAULT NULL COMMENT '收货时间',
+  `address_id` int(11) NULL DEFAULT 0 COMMENT '收货地址id',
+  `total_price` decimal(10, 2) NOT NULL COMMENT '总价',
+  `note` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '买家留言',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '订单表' ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Table structure for property
 -- ----------------------------
 DROP TABLE IF EXISTS `property`;
-CREATE TABLE `property` (
+CREATE TABLE `property`  (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `cateid` int(11) NOT NULL COMMENT '类别主键',
-  `proname` varchar(200) NOT NULL DEFAULT '' COMMENT '属性名称',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=62 DEFAULT CHARSET=utf8;
+  `cate_id` int(11) NOT NULL COMMENT '分类主键',
+  `property_name` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '属性名称',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '属性表' ROW_FORMAT = Compact;
 
 -- ----------------------------
--- Records of property
+-- Table structure for property_option
 -- ----------------------------
-INSERT INTO `property` VALUES ('1', '1', '品牌');
-INSERT INTO `property` VALUES ('2', '1', '版型');
-INSERT INTO `property` VALUES ('3', '1', '季节');
-INSERT INTO `property` VALUES ('4', '1', '面料');
-INSERT INTO `property` VALUES ('5', '1', '工艺处理');
-INSERT INTO `property` VALUES ('6', '1', '适用对象');
-INSERT INTO `property` VALUES ('7', '7', '品牌');
-INSERT INTO `property` VALUES ('8', '7', '型号');
-INSERT INTO `property` VALUES ('9', '7', '分辨率');
-INSERT INTO `property` VALUES ('10', '7', '核心数');
-INSERT INTO `property` VALUES ('11', '7', '运行内存');
-INSERT INTO `property` VALUES ('12', '7', '操作系统');
-INSERT INTO `property` VALUES ('13', '13', '品牌');
-INSERT INTO `property` VALUES ('14', '13', '版型');
-INSERT INTO `property` VALUES ('15', '13', '季节');
-INSERT INTO `property` VALUES ('16', '13', '面料');
-INSERT INTO `property` VALUES ('17', '13', '工艺处理');
-INSERT INTO `property` VALUES ('18', '13', '适用对象');
-INSERT INTO `property` VALUES ('19', '19', '品牌');
-INSERT INTO `property` VALUES ('20', '19', '型号');
-INSERT INTO `property` VALUES ('21', '19', '能效等级');
-INSERT INTO `property` VALUES ('22', '19', '款式');
-INSERT INTO `property` VALUES ('23', '19', '功率');
-INSERT INTO `property` VALUES ('24', '19', '保修期');
-INSERT INTO `property` VALUES ('26', '26', '生产日期');
-INSERT INTO `property` VALUES ('27', '26', '品牌');
-INSERT INTO `property` VALUES ('28', '26', '包装方式');
-INSERT INTO `property` VALUES ('29', '26', '产地');
-INSERT INTO `property` VALUES ('30', '26', '净含量');
-INSERT INTO `property` VALUES ('31', '26', '保质期');
-INSERT INTO `property` VALUES ('32', '32', '品牌');
-INSERT INTO `property` VALUES ('33', '32', '产地');
-INSERT INTO `property` VALUES ('34', '32', '型号');
-INSERT INTO `property` VALUES ('35', '32', '适用对象');
-INSERT INTO `property` VALUES ('36', '32', '保质期');
-INSERT INTO `property` VALUES ('37', '32', '是否为特殊用途化妆品');
-INSERT INTO `property` VALUES ('38', '38', '品牌');
-INSERT INTO `property` VALUES ('39', '38', '型号');
-INSERT INTO `property` VALUES ('40', '38', '材质');
-INSERT INTO `property` VALUES ('41', '38', '风格');
-INSERT INTO `property` VALUES ('42', '38', '毛重');
-INSERT INTO `property` VALUES ('43', '38', '是否可定制');
-INSERT INTO `property` VALUES ('44', '44', '品牌');
-INSERT INTO `property` VALUES ('45', '44', '质地');
-INSERT INTO `property` VALUES ('46', '44', '风格');
-INSERT INTO `property` VALUES ('47', '44', '性别');
-INSERT INTO `property` VALUES ('48', '44', '上市时间');
-INSERT INTO `property` VALUES ('49', '44', '大小');
-INSERT INTO `property` VALUES ('50', '51', '品牌');
-INSERT INTO `property` VALUES ('51', '51', '材质');
-INSERT INTO `property` VALUES ('52', '51', '适用性别');
-INSERT INTO `property` VALUES ('53', '51', '风格');
-INSERT INTO `property` VALUES ('54', '51', '镶嵌材质');
-INSERT INTO `property` VALUES ('55', '51', '上市时间');
-INSERT INTO `property` VALUES ('56', '0', '品种');
-INSERT INTO `property` VALUES ('57', '0', '原产地');
-INSERT INTO `property` VALUES ('58', '0', '最大体重');
-INSERT INTO `property` VALUES ('59', '0', '出生日期');
-INSERT INTO `property` VALUES ('60', '0', '性别');
-INSERT INTO `property` VALUES ('61', '0', '免疫驱虫');
+DROP TABLE IF EXISTS `property_option`;
+CREATE TABLE `property_option`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `property_id` int(11) NOT NULL COMMENT '属性id',
+  `option` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '选项值',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '属性选项表' ROW_FORMAT = Compact;
 
 -- ----------------------------
--- Table structure for propertyvalue
+-- Table structure for sku
 -- ----------------------------
-DROP TABLE IF EXISTS `propertyvalue`;
-CREATE TABLE `propertyvalue` (
+DROP TABLE IF EXISTS `sku`;
+CREATE TABLE `sku`  (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `proid` varchar(200) NOT NULL DEFAULT '0' COMMENT '商品主键',
-  `valueid` int(11) NOT NULL DEFAULT '0' COMMENT '属性id',
-  `provalue` varchar(200) NOT NULL DEFAULT '' COMMENT '属性值',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=74 DEFAULT CHARSET=utf8;
+  `old_price` decimal(10, 2) NULL DEFAULT NULL COMMENT '原价',
+  `new_price` decimal(10, 2) NOT NULL COMMENT '现价',
+  `stock` int(10) NOT NULL DEFAULT 0 COMMENT '库存',
+  `cover` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '商品图片路径',
+  `sales` int(255) NULL DEFAULT 0 COMMENT '月销售量',
+  `spu_id` int(11) NOT NULL COMMENT 'spu主键',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '商品sku表' ROW_FORMAT = Compact;
 
 -- ----------------------------
--- Table structure for store
+-- Table structure for specification
 -- ----------------------------
-DROP TABLE IF EXISTS `store`;
-CREATE TABLE `store` (
+DROP TABLE IF EXISTS `specification`;
+CREATE TABLE `specification`  (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `storename` varchar(200) DEFAULT '' COMMENT '店铺名字',
-  `userid` int(11) NOT NULL DEFAULT '0' COMMENT '店主id',
-  `info` varchar(500) NOT NULL DEFAULT '' COMMENT '店铺信息',
-  `storecover` varchar(200) NOT NULL DEFAULT '' COMMENT '店铺封面',
-  `buildtime` datetime DEFAULT NULL COMMENT '创建时间',
-  `typeid` int(11) NOT NULL DEFAULT '0' COMMENT '0代表开店，1代表已关店',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  `specification_name` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '规格名称',
+  `group_id` int(11) NOT NULL COMMENT '规格组主键',
+  `cate_id` int(11) NOT NULL COMMENT '类别主键',
+  `order` int(3) NULL DEFAULT NULL COMMENT '排序',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '规格表' ROW_FORMAT = Compact;
 
 -- ----------------------------
--- Table structure for userinfo
+-- Table structure for specification_group
 -- ----------------------------
-DROP TABLE IF EXISTS `userinfo`;
-CREATE TABLE `userinfo` (
+DROP TABLE IF EXISTS `specification_group`;
+CREATE TABLE `specification_group`  (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `username` varchar(200) DEFAULT '' COMMENT '用户名',
-  `password` varchar(500) DEFAULT '' COMMENT '密码',
-  `regtime` datetime DEFAULT NULL COMMENT '注册时间',
-  `openid` varchar(50) DEFAULT '' COMMENT '微信openid',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8;
+  `specification_group_name` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '规格组名称',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '规格组表' ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for specification_option
+-- ----------------------------
+DROP TABLE IF EXISTS `specification_option`;
+CREATE TABLE `specification_option`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `option_name` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '规格值',
+  `specification_id` int(11) NOT NULL COMMENT '规格主键',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '规格选项表' ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for specification_option_sku
+-- ----------------------------
+DROP TABLE IF EXISTS `specification_option_sku`;
+CREATE TABLE `specification_option_sku`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `option_id` int(11) NOT NULL COMMENT '规格选项主键',
+  `sku_id` int(11) NOT NULL COMMENT 'sku主键',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = 'sku-规格选项表' ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for spu
+-- ----------------------------
+DROP TABLE IF EXISTS `spu`;
+CREATE TABLE `spu`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `pro_name` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '商品名称',
+  `cate_id` int(11) NOT NULL DEFAULT 0 COMMENT '类别id，对应类别表id',
+  `store_id` int(11) NOT NULL DEFAULT 0 COMMENT '商铺id，对应商铺表id',
+  `status` int(1) NOT NULL DEFAULT 0 COMMENT '商品状态，0未上架，1已经上架',
+  `cover` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '商品图片路径',
+  `info` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '简介',
+  `note` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '详情',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '商品spu表' ROW_FORMAT = Compact;
+
+SET FOREIGN_KEY_CHECKS = 1;
